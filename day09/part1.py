6ad04123e650df6736fa631fb71bf99b96acf4a5
Joe <joe.hartshorn@zego.com>
2023-12-19 13:56:41 +0000

    day09

---
+++ day09/part1.py
@@ -0,0 +1,65 @@
+import numpy as np
+from scipy.special import factorial
+
+
+def parse_input(input_string: str) -> list[list[int]]:
+    return np.array(
+        [[int(num) for num in line.split()] for line in input_string.splitlines()]
+    )
+
+
+def orders(sequence):
+    while not np.all(sequence == 0):
+        yield sequence[0]
+        sequence = np.diff(sequence)
+
+
+def generate_term(power):
+    def term(n):
+        if power == 0:
+            return 1
+        elif power == 1:
+            return n
+        else:
+            return (1 / factorial(power)) * np.prod(np.arange(n - power + 1, n + 1))
+
+    return term
+
+
+def generate_polynomial(orders):
+    orders = list(orders)
+
+    def polynomial(n):
+        return sum(
+            order * generate_term(power)(n) for power, order in enumerate(orders)
+        )
+
+    return polynomial
+
+
+def get_next_number(sequence):
+    polynomial = generate_polynomial(orders(sequence))
+    generated_sequence = np.array(
+        [np.around(polynomial(n)) for n in range(0, len(sequence) + 1)]
+    )
+    assert np.all(
+        sequence == generated_sequence[:-1]
+    ), f"Generated sequence \n{generated_sequence} does not match input sequence \n{sequence}"
+    return int(generated_sequence[-1])
+
+
+if __name__ == "__main__":
+    with open("day09/input_example.txt") as f:
+        input_string = f.read()
+
+    sequences = parse_input(input_string)
+    next_values = [get_next_number(seq) for seq in sequences]
+    assert next_values == [18, 28, 68]
+    assert sum(next_values) == 114
+
+    with open("day09/input.txt") as f:
+        input_string = f.read()
+
+    sequences = parse_input(input_string)
+    next_values = [get_next_number(seq) for seq in sequences]
+    print(sum(next_values))
